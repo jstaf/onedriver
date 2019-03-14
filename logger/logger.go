f0bc8ca9fc0ed09aa7c299041d34f64dc2a83085
Jeff Stafford <jeff.stafford@protonmail.com>
2019-03-13 23:32:15 -0400

    loggerf is now logger

--- logger/logger.go
+++ logger/logger.go
@@ -42,9 +42,9 @@ func extractFuncName(ptr uintptr) string {
 	return fname[lastDot:] + "()"
 }
 
-// Log a function's output at a various level, ignoring those below the
+// Log a function's output at a various level, ignoring messages below the
 // currently configured level.
-func loggerf(level LogLevel, format string, args ...interface{}) {
+func logger(level LogLevel, format string, args ...interface{}) {
 	if level > currentLevel {
 		return
 	}
@@ -78,58 +78,54 @@ func loggerf(level LogLevel, format string, args ...interface{}) {
 		prefix, filepath.Base(file), line, functionName, preformatted)
 }
 
-func logger(level LogLevel, args ...interface{}) {
-	loggerf(level, "%s\n", args...)
-}
-
 // Fatalf logs and kills the program. Uses printf formatting.
 func Fatalf(format string, args ...interface{}) {
-	loggerf(FATAL, format, args...)
+	logger(FATAL, format, args...)
 	os.Exit(1)
 }
 
 // Fatal logs and kills the program
 func Fatal(args ...interface{}) {
-	logger(FATAL, args...)
+	logger(FATAL, "%s\n", args...)
 	os.Exit(1)
 }
 
 // Errorf logs at the Error level, but allows formatting
 func Errorf(format string, args ...interface{}) {
-	loggerf(ERROR, format, args...)
+	logger(ERROR, format, args...)
 }
 
 // Error logs at the Error level
 func Error(args ...interface{}) {
-	logger(ERROR, args...)
+	logger(ERROR, "%s\n", args...)
 }
 
 // Warnf logs at the Warn level, but allows formatting
 func Warnf(format string, args ...interface{}) {
-	loggerf(WARN, format, args...)
+	logger(WARN, format, args...)
 }
 
 // Warn logs at the Warn level
 func Warn(args ...interface{}) {
-	logger(WARN, args)
+	logger(WARN, "%s\n", args)
 }
 
 // Infof logs at the Info level, but allows formatting
 func Infof(format string, args ...interface{}) {
-	loggerf(INFO, format, args...)
+	logger(INFO, format, args...)
 }
 
 // Info logs at the Info level
 func Info(args ...interface{}) {
-	logger(INFO, args...)
+	logger(INFO, "%s\n", args...)
 }
 
 // Tracef logs at the Warn level, but allows formatting
 func Tracef(format string, args ...interface{}) {
-	loggerf(TRACE, format, args...)
+	logger(TRACE, format, args...)
 }
 
 // Trace logs at the Trace level
 func Trace(args ...interface{}) {
-	logger(TRACE, args...)
+	logger(TRACE, "%s\n", args...)
 }
