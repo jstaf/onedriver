0a24369ea4c242a3493845b35b05472bf0054da0
Jeff Stafford <jeff.stafford@protonmail.com>
2019-04-03 22:51:29 -0400

    more logger tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -46,6 +46,7 @@ func TestMultiplePrint(t *testing.T) {
 			contents, toMatch)
 	}
 }
+
 func TestPrintf(t *testing.T) {
 	temp, _ := ioutil.TempFile("", "logger_test_*")
 	log.SetOutput(temp)
@@ -60,5 +61,33 @@ func TestPrintf(t *testing.T) {
 	if !strings.Contains(contents, toMatch) {
 		t.Fatalf("Did not contain expected output.\nGot: \"%s\"\nWanted: \"%s\"\n",
 			contents, toMatch)
+	}
+}
+
+// does the logger contain values
+func TestLogLevel(t *testing.T) {
+	temp, _ := ioutil.TempFile("", "logger_test_*")
+	log.SetOutput(temp)
+	SetLogLevel(WARN)
+	defer os.Remove(temp.Name())
+	defer temp.Close()
+
+	// cant test fatal because it's... fatal to the test
+	Error("test")
+	Errorf("test")
+	Warn("test")
+	Warnf("test")
+	Info("test")
+	Infof("test")
+	Trace("test")
+	Tracef("test")
+
+	read, _ := ioutil.ReadFile(temp.Name())
+	contents := string(read)
+	if strings.Contains(contents, "INFO") || strings.Contains(contents, "TRACE") {
+		t.Fatal("Log contained log values below current level.")
+	}
+	if !strings.Contains(contents, "WARN") || !strings.Contains(contents, "ERROR") {
+		t.Fatal("Log did not contain values equal or above current level.")
 	}
 }
