ed7d204b07f791f2b4134953174d2605d57daae8
Jeff Stafford <jeff.stafford@protonmail.com>
2019-10-14 11:54:44 -0400

    create another failing delta test

--- graph/delta_test.go
+++ graph/delta_test.go
@@ -1,3 +1,4 @@
+// Run tests to verify that we are syncing changes from the server.
 package graph
 
 import (
@@ -7,7 +8,8 @@ import (
 	"time"
 )
 
-// Run tests to verify that we are syncing changes from the server.
+// In this test, we create a directory through the API, and wait to see if
+// the cache picks it up post-creation.
 func TestDeltaMkdir(t *testing.T) {
 	t.Parallel()
 	parent, err := GetItemPath("/onedriver_tests/delta", auth)
@@ -24,4 +26,27 @@ func TestDeltaMkdir(t *testing.T) {
 	if !st.Mode().IsDir() {
 		t.Fatalf("%s was not a directory!", filepath.Join(DeltaDir, "first"))
 	}
+}
+
+// We create a directory through the cache, then delete through the API and see
+// if the cache picks it up.
+func TestDeltaRmdir(t *testing.T) {
+	t.Parallel()
+	fname := filepath.Join(DeltaDir, "delete_me")
+	failOnErr(t, os.Mkdir(fname, 0755))
+
+	item, err := GetItemPath("/onedriver_tests/delta/delete_me", auth)
+	failOnErr(t, err)
+	failOnErr(t, Remove(item.ID(), auth))
+
+	// wait for delta sync
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second)
+		_, err := os.Stat(fname)
+		if err != nil {
+			// this is what we actually want
+			return
+		}
+	}
+	t.Fatal("File deletion not picked up by client")
 }
