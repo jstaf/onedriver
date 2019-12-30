b025bb1df70dced5d29b391e9095d269d067ed12
Jeff Stafford <jeff.stafford@protonmail.com>
2019-12-30 02:04:33 -0500

    fix travis (hopefully)

--- .travis_lint.sh
+++ .travis_lint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # checks formatting and other linting errors during tests.
+set -eu
 
-GOFMT_FAILURES="$(goimports -l .)"
+GOFMT_FAILURES="$($HOME/gopath/bin/goimports -l .)"
 if [ -n "$GOFMT_FAILURES" ]; then
     echo "The following files failed formatting checks (check against goimports):"
     echo "$GOFMT_FAILURES"
