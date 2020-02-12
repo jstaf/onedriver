8aa1ef8516975a21b2b7fe69ae6979b2317003f3
Jeff Stafford <jeff.stafford@protonmail.com>
2020-02-11 22:56:39 -0500

    no more global variables

--- graph/oauth2_gtk.c
+++ graph/oauth2_gtk.c
@@ -3,28 +3,25 @@
 #include <string.h>
 #include <webkit2/webkit2.h>
 
-static const char *auth_complete_url = "https://login.live.com/oauth20_desktop.srf";
-static char *auth_redirect_value = NULL;
-
 /**
- * Exit the main loop when the window is destroyed. 
+ * Exit the main loop when the window is destroyed.
  */
-static void destroy_window(GtkWidget *widget, gpointer data) {
-    gtk_main_quit();
-}
+static void destroy_window(GtkWidget *widget, gpointer data) { gtk_main_quit(); }
 
 /**
  * Catch redirects once authentication completes.
  */
-static void web_view_load_changed(WebKitWebView *web_view,
-                                  WebKitLoadEvent load_event,
-                                  gpointer data) {
-    const char *uri = webkit_web_view_get_uri(web_view);
+static void web_view_load_changed(WebKitWebView *web_view, WebKitLoadEvent load_event,
+                                  char *auth_redirect_url_ptr) {
+    static const char *auth_complete_url = "https://login.live.com/oauth20_desktop.srf";
+    const char *url = webkit_web_view_get_uri(web_view);
+
     if (load_event == WEBKIT_LOAD_REDIRECTED &&
-        strncmp(auth_complete_url, uri, strlen(auth_complete_url)) == 0) {
-        // catch redirects to the oauth2 redirect link only
-        auth_redirect_value = strdup(uri);
-        gtk_widget_destroy(GTK_WIDGET(data));
+        strncmp(auth_complete_url, url, strlen(auth_complete_url)) == 0) {
+        // catch redirects to the oauth2 redirect only and destroy the window
+        strncpy(auth_redirect_url_ptr, url, 2047);
+        GtkWidget *parent = gtk_widget_get_parent(GTK_WIDGET(web_view));
+        gtk_widget_destroy(parent);
     }
 }
 
@@ -41,18 +38,16 @@ char *webkit_auth_window(char *auth_url) {
     gtk_container_add(GTK_CONTAINER(auth_window), GTK_WIDGET(web_view));
     webkit_web_view_load_uri(web_view, auth_url);
 
+    char auth_redirect_value[2048];
+    auth_redirect_value[0] = '\0';
+    g_signal_connect(web_view, "load-changed", G_CALLBACK(web_view_load_changed),
+                     &auth_redirect_value);
     g_signal_connect(auth_window, "destroy", G_CALLBACK(destroy_window), web_view);
-    g_signal_connect(web_view, "load-changed", G_CALLBACK(web_view_load_changed), auth_window);
 
     // show and grab focus
     gtk_widget_grab_focus(GTK_WIDGET(web_view));
     gtk_widget_show_all(auth_window);
     gtk_main();
 
-    // go will explode if you return a null pointer
-    if (!auth_redirect_value) {
-        auth_redirect_value = "";
-    }
-
-    return auth_redirect_value;
+    return strdup(auth_redirect_value);
 }
