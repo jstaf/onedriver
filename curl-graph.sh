2f1a299707062f50bb93aabde9f3bc04be123638
Jeff Stafford <jeff.stafford@protonmail.com>
2020-03-22 23:12:26 -0400

    pull in logrus fix for go 1.14

--- curl-graph.sh
+++ curl-graph.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 TOKEN=$(jq -r .access_token $1)
-curl -H "Authorization: bearer $TOKEN" "https://graph.microsoft.com/v1.0$2"
+curl -s -H "Authorization: bearer $TOKEN" "https://graph.microsoft.com/v1.0$2"
