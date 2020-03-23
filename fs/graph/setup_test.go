4b7cc4b4cb06ed64e8c1eb0fa8af5b6bc4e68b2c
Jeff Stafford <jeff.stafford@protonmail.com>
2020-03-22 21:38:57 -0400

    log account dn + type in tests

--- fs/graph/setup_test.go
+++ fs/graph/setup_test.go
@@ -5,12 +5,22 @@ import (
 	"testing"
 
 	"github.com/jstaf/onedriver/logger"
+	log "github.com/sirupsen/logrus"
 )
 
 func TestMain(m *testing.M) {
 	os.Chdir("../..")
-	Authenticate(".auth_tokens.json")
 	f := logger.LogTestSetup()
 	defer f.Close()
+
+	// auth and log account metadata so we're extra sure who we're testing against
+	auth := Authenticate(".auth_tokens.json")
+	user, _ := GetUser(auth)
+	drive, _ := GetDrive(auth)
+	log.WithFields(log.Fields{
+		"account": user.UserPrincipalName,
+		"type":    drive.DriveType,
+	}).Info("Starting tests")
+
 	os.Exit(m.Run())
 }
