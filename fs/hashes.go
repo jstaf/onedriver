a7798f9e8c75ffb00128e2fcf945c3f1704f1bc0
Jeff Stafford <jeff.stafford@protonmail.com>
2020-06-03 17:45:57 -0400

    sha1 hashes are now formatted in all caps

--- fs/hashes.go
+++ fs/hashes.go
@@ -4,13 +4,15 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
+	"strings"
 
 	"github.com/rclone/rclone/backend/onedrive/quickxorhash"
 )
 
 // SHA1Hash returns the SHA1 hash of some data as a string
 func SHA1Hash(data *[]byte) string {
-	return fmt.Sprintf("%x", sha1.Sum(*data))
+	// the onedrive API returns SHA1 hashes in all caps, so we do too
+	return strings.ToUpper(fmt.Sprintf("%x", sha1.Sum(*data)))
 }
 
 // QuickXORHash computes the Microsoft-specific QuickXORHash. Reusing rclone's
