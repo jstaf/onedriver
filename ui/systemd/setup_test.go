92c75ad08b86f9d655064473b2152fe47690e49e
Jeff Stafford <jeff.stafford@protonmail.com>
2022-01-29 13:44:04 -0500

    systemd tests now use testify

--- ui/systemd/setup_test.go
+++ ui/systemd/setup_test.go
@@ -2,7 +2,6 @@ package systemd
 
 import (
 	"os"
-	"runtime"
 	"testing"
 )
 
@@ -10,13 +9,4 @@ func TestMain(m *testing.M) {
 	os.Chdir("../..")
 	os.Mkdir("mount", 0700)
 	os.Exit(m.Run())
-}
-
-// convenience handler to fail tests if an error is not nil
-func failOnErr(t *testing.T, err error) {
-	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		t.Logf("Test failed at %s:%d:\n", file, line)
-		t.Fatal(err)
-	}
 }
