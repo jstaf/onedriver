55eddbedfd1989e96d7b4ab0608f8d86b9d519fe
Jeff Stafford <jeff.stafford@protonmail.com>
2023-07-14 00:57:45 -0400

    disable cgo helper if build has cgo disabled

--- cgo-helper.sh
+++ cgo-helper.sh
@@ -3,6 +3,10 @@
 # are installed so this script is here to autodetect which webkit2gtk c headers
 # we have access to
 
+if [ "$CGO_ENABLED" -eq 0 ]; then
+    exit 0
+fi
+
 if pkg-config webkit2gtk-4.0; then
     sed -i 's/webkit2gtk-4.1/webkit2gtk-4.0/g' fs/graph/oauth2_gtk.go
 elif ! pkg-config webkit2gtk-4.1; then
