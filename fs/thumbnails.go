401a83a595392a8950fe3d42e0cabc963bf01582
Jeff Stafford <jeff.stafford@protonmail.com>
2023-07-27 01:00:02 -0400

    fix thumbnail path calculation

--- fs/thumbnails.go
+++ fs/thumbnails.go
@@ -3,11 +3,28 @@ package fs
 import (
 	"crypto/md5"
 	"fmt"
+	"os"
 )
 
-// ThumbnailPath generates the path for local thumbnail
-func (f *Filesystem) ThumbnailPath(path string) string {
-	//TODO fixme
-	uri := fmt.Sprintf("file://%s/%s", f.content.directory, path)
+const (
+	ThumbnailSizeLarge  = "large"
+	ThumbnailSizeNormal = "normal"
+	ThumbnailSizeFail   = "fail/gnome-thumbnail-factory"
+)
+
+type Thumbnailer struct {
+	mountpoint string
+}
+
+// HashURI hashes a URI like the Freedesktop standard expects
+// https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html
+func HashURI(uri string) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(uri)))
+}
+
+// ThumbnailPath generates the path for local thumbnail
+func (t Thumbnailer) ThumbnailPath(path string, thumbnailSize string) string {
+	cacheDir, _ := os.UserCacheDir()
+	uri := fmt.Sprintf("file://%s%s", t.mountpoint, path)
+	return fmt.Sprintf("%s/thumbnails/%s/%s.jpg", cacheDir, thumbnailSize, HashURI(uri))
 }
--- fs/thumbnails_test.go
+++ fs/thumbnails_test.go
@@ -1,14 +1,33 @@
 package fs
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestThumbnailPath(t *testing.T) {
+func TestHashURI(t *testing.T) {
 	require.Equal(t,
-		"file:///home/jstaf/OneDrive/ginger.jpg",
-		"fa0e529cb193135a04bdd3ec4ced1f44.png",
+		HashURI("file:///home/jstaf/OneDrive/ginger.jpg"),
+		"fa0e529cb193135a04bdd3ec4ced1f44",
+	)
+}
+
+func TestThumbnailPath(t *testing.T) {
+	onedriveFilePath := "/ginger.jpg"
+	thumbnailer := Thumbnailer{mountpoint: "/tmp/test"}
+	mountedFilePath := fmt.Sprintf("file://%s%s", thumbnailer.mountpoint, onedriveFilePath)
+
+	cacheDir, _ := os.UserCacheDir()
+	require.Equal(t,
+		fmt.Sprintf(
+			"%s/thumbnails/%s/%s.jpg",
+			cacheDir,
+			ThumbnailSizeLarge,
+			HashURI(mountedFilePath),
+		),
+		thumbnailer.ThumbnailPath(onedriveFilePath, ThumbnailSizeLarge),
 	)
 }
