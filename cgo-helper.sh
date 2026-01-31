c5a87dfcadc20a16d427abcf77e604f6e26ef50b
Jeff Stafford <jeff.stafford@protonmail.com>
2026-01-31 11:18:08 -0800

    Fix debian changelog, fix rocky 9 builds (#462)
    * fix line formatting for debian
    
    * fix rocky 9 builds

--- cgo-helper.sh
+++ cgo-helper.sh
@@ -7,7 +7,9 @@ if [ -n "$CGO_ENABLED" ] && [ "$CGO_ENABLED" -eq 0 ]; then
     exit 0
 fi
 
-if ! pkg-config webkit2gtk-4.1; then
+if pkg-config webkit2gtk-4.0; then
+    sed -i 's/webkit2gtk-4.1/webkit2gtk-4.0/g' fs/graph/oauth2_gtk.go
+elif ! pkg-config webkit2gtk-4.1; then
     echo "webkit2gtk development headers must be installed"
     exit 1
 fi
